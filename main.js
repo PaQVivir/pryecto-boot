f0442975ff5ea8cb9ea9a380e012bb10670c8bf9
Leonardo Fernandez <leonardomartinfer@gmail.com>
2023-03-28 21:02:04 -0300

    tercera pre entrega

---
+++ main.js
@@ -0,0 +1,10 @@
+//let email = prompt ("Ingresa tu E-mail");
+//let birthday = prompt("Ingresa tu fecha de cumpleaños")
+
+//if ((email) && (birthday)) {
+//alert("Genial..., pronto recibiras excelentes noticias")
+//} else {
+//alert("Lastima, te vas a perder de las ofertas")
+//}
+
+
